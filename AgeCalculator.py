59fd1fd3e090783dd1cdccdf974a589c4a93837e
Anshul Prajapati <parjapatianshul695@gmail.com>
2025-06-30 10:43:12 +0530

    fixed the bugs

--- AgeCalculator.py
+++ AgeCalculator.py
@@ -1,43 +1,52 @@
 #-----------------------Age Calculator-----------------------
-from datetime import date 
-import time
+import calendar
+from datetime import datetime
 
 #--------------------------main logic------------------------
-#________to calculate years________
-def year_calculate():
-    print(f"******WLECOME TO AGE CALCULATOR*******")
-    birth_year=int(input("Enter your birth year(e.g. 2005) : "))
-    current_year=date.today().year
-    year= current_year - birth_year
-    return year
+#________to calculate age________
+def calculate_age(birth_date,current_date):
+    #_____________to calculate years____________
+    years=(current_date.year - birth_date.year) - 1
+    #____________to calculate months_____________
+    m=current_date.month - birth_date.month
+    if m<0:
+        month=12+m
+    else:
+        month=12-m
+    #___________to calculate days_______________
+    d=current_date.day - birth_date.day
+    if d<0:
+        #get previous month and year
+        if current_date.month == 1:
+            pre_month=12
+            year=current_date.year - 1
+        else:
+            pre_month=current_date.month - 1
+            year=current_date.year
 
-#_______to calculate months________
-def month_calculate():
-    month=date.today().month - 1
-    return month
+        #get number of days in previous month
+        days_in_pre_month=calendar.monthrange(year,pre_month)[1]
+        day=days_in_pre_month + d
+    else:
+        day=d
+    
+    return years,month,day 
 
-#_______to calculate days__________
-def day_calculate():
-    day=date.today().day - 1
-    return day
-
-#______to calculate hours__________
-def hour_calculate():
-    hour=time.localtime().tm_hour
-    return hour
-
-#______to calculate minutes_________
-def minute_calculate():
-    minute=time.localtime().tm_min
-    return minute
-
-#______to calculate seconds_________
-def second_calculate():
-    second=time.localtime().tm_sec
-    return second
-
-#--------------------------function call---------------------
+#--------------------------input from user---------------------
+print(f"******WLECOME TO AGE CALCULATOR*******")
 name=input("Enter your name : ")
-print(f"HELLO! {name}, you are {year_calculate()} years {month_calculate()} months {day_calculate()} days {hour_calculate()} hours {minute_calculate()} minutes and {second_calculate()} seconds old")
+birth_input=input("Enter your date of birth in (YYYY-MM-DD) format : ")
+try:
+    birth_date=datetime.strptime(birth_input,"%Y-%m-%d")
+    current_date=datetime.now()
+
+    if birth_date>current_date:
+        print("Birth date can not be in future")
+    else:
+        y,m,d=calculate_age(birth_date,current_date)
+#__________________________display output_____________________
+        print(f"HELLO! {name}, you are {y} years {m} months and {d} days old")
+except ValueError:
+    print("Invalid format Please enter in (YYYY-MM-DD) format!")
 
 #--------------------------END OF PROGRAM--------------------
